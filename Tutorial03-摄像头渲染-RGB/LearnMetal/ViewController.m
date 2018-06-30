f8d41aeef68e88c94c2a3af459a8f708e1c0d554
loyinglin <loying@foxmail.com>
2018-06-30 17:02:13 +0800

    摄像头采集

--- Tutorial03-摄像头渲染-RGB/LearnMetal/ViewController.m
+++ Tutorial03-摄像头渲染-RGB/LearnMetal/ViewController.m
@@ -29,7 +29,6 @@
 
 
 // data
-@property (nonatomic, assign) vector_uint2 viewportSize;
 @property (nonatomic, strong) id<MTLCommandQueue> commandQueue;
 @property (nonatomic, strong) id<MTLTexture> texture;
 
@@ -43,44 +42,37 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     // 设置Metal 相关
-    self.mtkView = [[MTKView alloc] initWithFrame:self.view.bounds];
-    self.mtkView.device = MTLCreateSystemDefaultDevice();
-    [self.view insertSubview:self.mtkView atIndex:0];
-    self.mtkView.delegate = self;
-    self.mtkView.framebufferOnly = NO;
-//    self.mtkView.transform = CGAffineTransformMakeRotation(M_PI / 2);
-    self.viewportSize = (vector_uint2){self.mtkView.drawableSize.width, self.mtkView.drawableSize.height};
-    self.commandQueue = [self.mtkView.device newCommandQueue];
-    CVMetalTextureCacheCreate(NULL, NULL, self.mtkView.device, NULL, &_textureCache);
-    
+    [self setupMetal];
     // 设置采集相关
     [self setupCaptureSession];
 }
 
+- (void)setupMetal {
+    self.mtkView = [[MTKView alloc] initWithFrame:self.view.bounds];
+    self.mtkView.device = MTLCreateSystemDefaultDevice();
+    [self.view insertSubview:self.mtkView atIndex:0];
+    self.mtkView.delegate = self;
+    self.mtkView.framebufferOnly = NO; // 允许读写操作
+    //    self.mtkView.transform = CGAffineTransformMakeRotation(M_PI / 2);
+    self.commandQueue = [self.mtkView.device newCommandQueue];
+    CVMetalTextureCacheCreate(NULL, NULL, self.mtkView.device, NULL, &_textureCache);
+}
+
 - (void)setupCaptureSession {
-    
     self.mCaptureSession = [[AVCaptureSession alloc] init];
     self.mCaptureSession.sessionPreset = AVCaptureSessionPreset1920x1080;
-    
-    self.mProcessQueue = dispatch_queue_create("mProcessQueue", DISPATCH_QUEUE_SERIAL);
-    
+    self.mProcessQueue = dispatch_queue_create("mProcessQueue", DISPATCH_QUEUE_SERIAL); // 串行队列
     AVCaptureDevice *inputCamera = nil;
     NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
-    for (AVCaptureDevice *device in devices)
-    {
-        if ([device position] == AVCaptureDevicePositionBack)
-        {
+    for (AVCaptureDevice *device in devices) {
+        if ([device position] == AVCaptureDevicePositionBack) {
             inputCamera = device;
         }
     }
-    
     self.mCaptureDeviceInput = [[AVCaptureDeviceInput alloc] initWithDevice:inputCamera error:nil];
-    
     if ([self.mCaptureSession canAddInput:self.mCaptureDeviceInput]) {
         [self.mCaptureSession addInput:self.mCaptureDeviceInput];
     }
-    
-    
     self.mCaptureDeviceOutput = [[AVCaptureVideoDataOutput alloc] init];
     [self.mCaptureDeviceOutput setAlwaysDiscardsLateVideoFrames:NO];
     // 这里设置格式为BGRA，而不用YUV的颜色空间，避免使用Shader转换
@@ -89,25 +81,20 @@
     if ([self.mCaptureSession canAddOutput:self.mCaptureDeviceOutput]) {
         [self.mCaptureSession addOutput:self.mCaptureDeviceOutput];
     }
-    
     AVCaptureConnection *connection = [self.mCaptureDeviceOutput connectionWithMediaType:AVMediaTypeVideo];
-    [connection setVideoOrientation:AVCaptureVideoOrientationPortrait];
-    
+    [connection setVideoOrientation:AVCaptureVideoOrientationPortrait]; // 设置方向
     [self.mCaptureSession startRunning];
-    
 }
 
 #pragma mark - delegate
 
-- (void)mtkView:(MTKView *)view drawableSizeWillChange:(CGSize)size {
-    self.viewportSize = (vector_uint2){size.width, size.height};
+- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size {
+    
 }
 
 - (void)drawInMTKView:(MTKView *)view {
-    
     if (self.texture) {
         id<MTLCommandBuffer> commandBuffer = [self.commandQueue commandBuffer]; // 创建指令缓冲
-        
         id<MTLTexture> drawingTexture = view.currentDrawable.texture; // 把MKTView作为目标纹理
         
         MPSImageGaussianBlur *filter = [[MPSImageGaussianBlur alloc] initWithDevice:self.mtkView.device sigma:1]; // 这里的sigma值可以修改，sigma值越高图像越模糊
