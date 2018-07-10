bafeaf08e09004307d634d2b8c54432c03e5c5f6
loyinglin <loying@foxmail.com>
2018-07-10 21:02:22 +0800

    滤镜链。

---
+++ tmp-滤镜链/LearnMetal/GL/LYOpenGLView.m
@@ -0,0 +1,417 @@
+//
+//  Created by loyinglin on 2018年06月29日.
+//  Copyright © 2018年 loyinglin. All rights reserved.
+//
+
+#import "LYOpenGLView.h"
+#import <QuartzCore/QuartzCore.h>
+#import <AVFoundation/AVUtilities.h>
+#import <mach/mach_time.h>
+#import <GLKit/GLKit.h>
+
+// Uniform index.
+enum
+{
+	UNIFORM_INPUT,
+	NUM_UNIFORMS
+};
+GLint uniforms[NUM_UNIFORMS];
+
+// Attribute index.
+enum
+{
+	ATTRIB_VERTEX,
+	ATTRIB_TEXCOORD,
+	NUM_ATTRIBUTES
+};
+
+@interface LYOpenGLView ()
+{
+	// The pixel dimensions of the CAEAGLLayer.
+	GLint _backingWidth;
+	GLint _backingHeight;
+
+	EAGLContext *_context;
+	CVOpenGLESTextureRef _inputTexture;
+	CVOpenGLESTextureCacheRef _textureCache;
+	
+	GLuint _frameBufferHandle;
+	GLuint _colorBufferHandle;
+}
+
+@property GLuint program;
+
+- (void)setupBuffers;
+- (void)cleanUpTextures;
+
+- (BOOL)loadShaders;
+- (BOOL)compileShader:(GLuint *)shader type:(GLenum)type URL:(NSURL *)URL;
+- (BOOL)linkProgram:(GLuint)prog;
+- (BOOL)validateProgram:(GLuint)prog;
+
+@end
+
+@implementation LYOpenGLView
+
++ (Class)layerClass
+{
+	return [CAEAGLLayer class];
+}
+
+- (id)initWithFrame:(CGRect)frame {
+	if (self = [super initWithFrame:frame])
+	{
+		self.contentScaleFactor = [[UIScreen mainScreen] scale];
+
+		CAEAGLLayer *eaglLayer = (CAEAGLLayer *)self.layer;
+
+		eaglLayer.opaque = NO;
+		eaglLayer.drawableProperties = @{ kEAGLDrawablePropertyRetainedBacking :[NSNumber numberWithBool:NO],
+										  kEAGLDrawablePropertyColorFormat : kEAGLColorFormatRGBA8};
+
+		_context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+
+		if (!_context || ![EAGLContext setCurrentContext:_context] || ![self loadShaders]) {
+			return nil;
+		}
+		
+	}
+	return self;
+}
+
+# pragma mark - OpenGL setup
+
+- (void)setupGL
+{
+	[EAGLContext setCurrentContext:_context];
+	[self setupBuffers];
+	[self loadShaders];
+	
+	glUseProgram(self.program);
+	
+	glUniform1i(uniforms[UNIFORM_INPUT], 0);
+	
+	// Create CVOpenGLESTextureCacheRef for optimal CVPixelBufferRef to GLES texture conversion.
+	if (!_textureCache) {
+		CVReturn err = CVOpenGLESTextureCacheCreate(kCFAllocatorDefault, NULL, _context, NULL, &_textureCache);
+		if (err != noErr) {
+			NSLog(@"Error at CVOpenGLESTextureCacheCreate %d", err);
+			return;
+		}
+	}
+}
+
+#pragma mark - Utilities
+
+- (void)setupBuffers
+{
+	glDisable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	
+	glEnableVertexAttribArray(ATTRIB_VERTEX);
+	glVertexAttribPointer(ATTRIB_VERTEX, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
+	
+	glEnableVertexAttribArray(ATTRIB_TEXCOORD);
+	glVertexAttribPointer(ATTRIB_TEXCOORD, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), 0);
+	
+	glGenFramebuffers(1, &_frameBufferHandle);
+	glBindFramebuffer(GL_FRAMEBUFFER, _frameBufferHandle);
+	
+	glGenRenderbuffers(1, &_colorBufferHandle);
+	glBindRenderbuffer(GL_RENDERBUFFER, _colorBufferHandle);
+	
+	[_context renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer *)self.layer];
+	glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &_backingWidth);
+	glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &_backingHeight);
+
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _colorBufferHandle);
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		NSLog(@"Failed to make complete framebuffer object %x", glCheckFramebufferStatus(GL_FRAMEBUFFER));
+	}
+}
+
+- (void)cleanUpTextures
+{
+	if (_inputTexture) {
+		CFRelease(_inputTexture);
+		_inputTexture = NULL;
+	}
+	
+	// Periodic texture cache flush every frame
+	CVOpenGLESTextureCacheFlush(_textureCache, 0);
+}
+
+- (void)dealloc
+{
+	[self cleanUpTextures];
+	
+	if(_textureCache) {
+		CFRelease(_textureCache);
+	}
+}
+
+#pragma mark - OpenGLES drawing
+
+- (void)displayPixelBuffer:(CVPixelBufferRef)pixelBuffer
+{
+	CVReturn err;
+	if (pixelBuffer != NULL) {
+		int frameWidth = (int)CVPixelBufferGetWidth(pixelBuffer);
+		int frameHeight = (int)CVPixelBufferGetHeight(pixelBuffer);
+		
+		if (!_textureCache) {
+			NSLog(@"No video texture cache");
+			return;
+		}
+        if ([EAGLContext currentContext] != _context) {
+            [EAGLContext setCurrentContext:_context]; // 非常重要的一行代码
+        }
+		[self cleanUpTextures];
+		
+					
+		/*
+         CVOpenGLESTextureCacheCreateTextureFromImage will create GLES texture optimally from CVPixelBufferRef.
+         */
+		
+		glActiveTexture(GL_TEXTURE0);
+		err = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
+														   _textureCache,
+														   pixelBuffer,
+														   NULL,
+														   GL_TEXTURE_2D,
+														   GL_RGBA,
+														   frameWidth,
+														   frameHeight,
+														   GL_RGBA,
+														   GL_UNSIGNED_BYTE,
+														   0,
+														   &_inputTexture);
+		if (err) {
+			NSLog(@"Error at CVOpenGLESTextureCacheCreateTextureFromImage %d", err);
+		}
+		
+        glBindTexture(CVOpenGLESTextureGetTarget(_inputTexture), CVOpenGLESTextureGetName(_inputTexture));
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		
+        
+		glBindFramebuffer(GL_FRAMEBUFFER, _frameBufferHandle);
+		
+		// Set the view port to the entire view.
+		glViewport(0, 0, _backingWidth, _backingHeight);
+	}
+	
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	// Use shader program.
+	glUseProgram(self.program);
+	
+	// Set up the quad vertices with respect to the orientation and aspect ratio of the video.
+	CGRect vertexSamplingRect = AVMakeRectWithAspectRatioInsideRect(CGSizeMake(_backingWidth, _backingHeight), self.layer.bounds);
+	
+	// Compute normalized quad coordinates to draw the frame into.
+	CGSize normalizedSamplingSize = CGSizeMake(0.0, 0.0);
+	CGSize cropScaleAmount = CGSizeMake(vertexSamplingRect.size.width/self.layer.bounds.size.width, vertexSamplingRect.size.height/self.layer.bounds.size.height);
+	
+	// Normalize the quad vertices.
+	if (cropScaleAmount.width > cropScaleAmount.height) {
+		normalizedSamplingSize.width = 1.0;
+		normalizedSamplingSize.height = cropScaleAmount.height/cropScaleAmount.width;
+	}
+	else {
+		normalizedSamplingSize.width = 1.0;
+		normalizedSamplingSize.height = cropScaleAmount.width/cropScaleAmount.height;
+	}
+	
+	/*
+     The quad vertex data defines the region of 2D plane onto which we draw our pixel buffers.
+     Vertex data formed using (-1,-1) and (1,1) as the bottom left and top right coordinates respectively, covers the entire screen.
+     */
+	GLfloat quadVertexData [] = {
+		-1 * normalizedSamplingSize.width, -1 * normalizedSamplingSize.height,
+			 normalizedSamplingSize.width, -1 * normalizedSamplingSize.height,
+		-1 * normalizedSamplingSize.width, normalizedSamplingSize.height,
+			 normalizedSamplingSize.width, normalizedSamplingSize.height,
+	};
+	
+	// 更新顶点数据
+	glVertexAttribPointer(ATTRIB_VERTEX, 2, GL_FLOAT, 0, 0, quadVertexData);
+	glEnableVertexAttribArray(ATTRIB_VERTEX);
+    
+    GLfloat quadTextureData[] =  { // 正常坐标
+        0.0, 1.0,
+        1.0, 1.0,
+        0.0, 0.0,
+        1.0, 0.0
+    };
+	
+	glVertexAttribPointer(ATTRIB_TEXCOORD, 2, GL_FLOAT, 0, 0, quadTextureData);
+	glEnableVertexAttribArray(ATTRIB_TEXCOORD);
+	
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+	glBindRenderbuffer(GL_RENDERBUFFER, _colorBufferHandle);
+    
+    if ([EAGLContext currentContext] == _context) {
+        [_context presentRenderbuffer:GL_RENDERBUFFER];
+    }
+    
+}
+
+#pragma mark -  OpenGL ES 2 shader compilation
+
+- (BOOL)loadShaders
+{
+	GLuint vertShader, fragShader;
+	NSURL *vertShaderURL, *fragShaderURL;
+	
+	self.program = glCreateProgram();
+	
+	// Create and compile the vertex shader.
+	vertShaderURL = [[NSBundle mainBundle] URLForResource:@"Shader" withExtension:@"vsh"];
+	if (![self compileShader:&vertShader type:GL_VERTEX_SHADER URL:vertShaderURL]) {
+		NSLog(@"Failed to compile vertex shader");
+		return NO;
+	}
+	
+	// Create and compile fragment shader.
+	fragShaderURL = [[NSBundle mainBundle] URLForResource:@"Shader" withExtension:@"fsh"];
+	if (![self compileShader:&fragShader type:GL_FRAGMENT_SHADER URL:fragShaderURL]) {
+		NSLog(@"Failed to compile fragment shader");
+		return NO;
+	}
+	
+	// Attach vertex shader to program.
+	glAttachShader(self.program, vertShader);
+	
+	// Attach fragment shader to program.
+	glAttachShader(self.program, fragShader);
+	
+	// Bind attribute locations. This needs to be done prior to linking.
+	glBindAttribLocation(self.program, ATTRIB_VERTEX, "position");
+	glBindAttribLocation(self.program, ATTRIB_TEXCOORD, "texCoord");
+	
+	// Link the program.
+	if (![self linkProgram:self.program]) {
+		NSLog(@"Failed to link program: %d", self.program);
+		
+		if (vertShader) {
+			glDeleteShader(vertShader);
+			vertShader = 0;
+		}
+		if (fragShader) {
+			glDeleteShader(fragShader);
+			fragShader = 0;
+		}
+		if (self.program) {
+			glDeleteProgram(self.program);
+			self.program = 0;
+		}
+		
+		return NO;
+	}
+	
+	// Get uniform locations.
+	uniforms[UNIFORM_INPUT] = glGetUniformLocation(self.program, "texture");
+	
+	// Release vertex and fragment shaders.
+	if (vertShader) {
+		glDetachShader(self.program, vertShader);
+		glDeleteShader(vertShader);
+	}
+	if (fragShader) {
+		glDetachShader(self.program, fragShader);
+		glDeleteShader(fragShader);
+	}
+	
+	return YES;
+}
+
+- (BOOL)compileShader:(GLuint *)shader type:(GLenum)type URL:(NSURL *)URL
+{
+    NSError *error;
+    NSString *sourceString = [[NSString alloc] initWithContentsOfURL:URL encoding:NSUTF8StringEncoding error:&error];
+    if (sourceString == nil) {
+		NSLog(@"Failed to load vertex shader: %@", [error localizedDescription]);
+        return NO;
+    }
+    
+	GLint status;
+	const GLchar *source;
+	source = (GLchar *)[sourceString UTF8String];
+	
+	*shader = glCreateShader(type);
+	glShaderSource(*shader, 1, &source, NULL);
+	glCompileShader(*shader);
+	
+#if defined(DEBUG)
+	GLint logLength;
+	glGetShaderiv(*shader, GL_INFO_LOG_LENGTH, &logLength);
+	if (logLength > 0) {
+		GLchar *log = (GLchar *)malloc(logLength);
+		glGetShaderInfoLog(*shader, logLength, &logLength, log);
+		NSLog(@"Shader compile log:\n%s", log);
+		free(log);
+	}
+#endif
+	
+	glGetShaderiv(*shader, GL_COMPILE_STATUS, &status);
+	if (status == 0) {
+		glDeleteShader(*shader);
+		return NO;
+	}
+	
+	return YES;
+}
+
+- (BOOL)linkProgram:(GLuint)prog
+{
+	GLint status;
+	glLinkProgram(prog);
+	
+#if defined(DEBUG)
+	GLint logLength;
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &logLength);
+	if (logLength > 0) {
+		GLchar *log = (GLchar *)malloc(logLength);
+		glGetProgramInfoLog(prog, logLength, &logLength, log);
+		NSLog(@"Program link log:\n%s", log);
+		free(log);
+	}
+#endif
+	
+	glGetProgramiv(prog, GL_LINK_STATUS, &status);
+	if (status == 0) {
+		return NO;
+	}
+	
+	return YES;
+}
+
+- (BOOL)validateProgram:(GLuint)prog
+{
+	GLint logLength, status;
+	
+	glValidateProgram(prog);
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &logLength);
+	if (logLength > 0) {
+		GLchar *log = (GLchar *)malloc(logLength);
+		glGetProgramInfoLog(prog, logLength, &logLength, log);
+		NSLog(@"Program validate log:\n%s", log);
+		free(log);
+	}
+	
+	glGetProgramiv(prog, GL_VALIDATE_STATUS, &status);
+	if (status == 0) {
+		return NO;
+	}
+	
+	return YES;
+}
+
+@end
+
---
+++ tmp-滤镜链/LearnMetal/ViewController.m
@@ -0,0 +1,318 @@
+//
+//  ViewController.m
+//  LearnMetal
+//
+//  Created by loyinglin on 2018/6/21.
+//  Copyright © 2018年 loyinglin. All rights reserved.
+//
+@import MetalKit;
+#import "LYShaderTypes.h"
+#import "ViewController.h"
+#import "LYOpenGLView.h"
+
+@interface ViewController () <MTKViewDelegate>
+
+// view
+@property (nonatomic, strong) MTKView *mtkView;
+@property (nonatomic, strong) UIImageView *imageView;
+
+
+// GL
+@property (nonatomic, strong) LYOpenGLView *glView;
+
+
+// data
+@property (nonatomic, assign) vector_uint2 viewportSize;
+@property (nonatomic, strong) id<MTLRenderPipelineState> renderPipelineState;
+@property (nonatomic, strong) id<MTLCommandQueue> commandQueue;
+@property (nonatomic, strong) id<MTLTexture> sourceTexture;
+@property (nonatomic, strong) id<MTLTexture> destTexture;
+@property (nonatomic, assign) CVMetalTextureCacheRef textureCache;
+@property (nonatomic, assign) CVPixelBufferRef renderPixelBuffer;
+@property (nonatomic, strong) MTLRenderPassDescriptor *renderPassDescriptor;
+
+
+@property (nonatomic, strong) id<MTLBuffer> vertices;
+@property (nonatomic, assign) NSUInteger numVertices;
+@property (nonatomic, assign) MTLSize groupSize;
+@property (nonatomic, assign) MTLSize groupCount;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 初始化 MTKView
+    self.mtkView = [[MTKView alloc] initWithFrame:self.view.bounds];
+    self.mtkView.device = MTLCreateSystemDefaultDevice(); // 获取默认的device
+    self.view = self.mtkView;
+    self.mtkView.delegate = self;
+    self.viewportSize = (vector_uint2){self.mtkView.drawableSize.width, self.mtkView.drawableSize.height};
+    
+    self.glView = [[LYOpenGLView alloc] initWithFrame:CGRectMake(CGRectGetWidth(self.view.bounds) / 2, 0, CGRectGetWidth(self.view.bounds) / 2, CGRectGetHeight(self.view.bounds) / 2)];
+    [self.glView setupGL];
+    [self.view addSubview:self.glView];
+    
+    [self customInit];
+}
+
+- (void)customInit {
+    [self setupPipeline];
+    [self setupVertex];
+    [self setupTexture];
+    [self setupRenderTarget];
+    [self setupThreadGroup];
+}
+
+// 设置渲染管道和计算管道
+-(void)setupPipeline {
+    id<MTLLibrary> defaultLibrary = [self.mtkView.device newDefaultLibrary]; // .metal
+    id<MTLFunction> vertexFunction = [defaultLibrary newFunctionWithName:@"vertexShader"]; // 顶点shader，vertexShader是函数名
+    id<MTLFunction> fragmentFunction = [defaultLibrary newFunctionWithName:@"samplingShader"]; // 片元shader，samplingShader是函数名
+    
+    MTLRenderPipelineDescriptor *pipelineStateDescriptor = [[MTLRenderPipelineDescriptor alloc] init];
+    pipelineStateDescriptor.vertexFunction = vertexFunction;
+    pipelineStateDescriptor.fragmentFunction = fragmentFunction;
+
+    pipelineStateDescriptor.colorAttachments[0].pixelFormat = self.mtkView.colorPixelFormat;
+    pipelineStateDescriptor.colorAttachments[1].pixelFormat = MTLPixelFormatBGRA8Unorm;
+    
+    NSError *error;
+    // 创建图形渲染管道，耗性能操作不宜频繁调用
+    self.renderPipelineState = [self.mtkView.device newRenderPipelineStateWithDescriptor:pipelineStateDescriptor
+                                                                                   error:&error];
+    // CommandQueue是渲染指令队列，保证渲染指令有序地提交到GPU
+    self.commandQueue = [self.mtkView.device newCommandQueue];
+    
+    
+    self.renderPassDescriptor = [MTLRenderPassDescriptor new];
+    self.renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0, 0.5, 0.5, 1.0);
+    self.renderPassDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
+    self.renderPassDescriptor.colorAttachments[0].storeAction = MTLStoreActionStore;
+    
+    self.renderPassDescriptor.colorAttachments[1].clearColor = MTLClearColorMake(0, 0, 0, 0);
+    self.renderPassDescriptor.colorAttachments[1].loadAction = MTLLoadActionClear;
+    self.renderPassDescriptor.colorAttachments[1].storeAction = MTLStoreActionStore;
+}
+
+- (void)setupVertex {
+    const LYVertex quadVertices[] =
+    {   // 顶点坐标，分别是x、y、z、w；    纹理坐标，x、y；
+        { {  0.5, -0.5/self.viewportSize.y*self.viewportSize.x, 0.0, 1.0 },  { 1.f, 1.f } },
+        { { -0.5, -0.5/self.viewportSize.y*self.viewportSize.x, 0.0, 1.0 },  { 0.f, 1.f } },
+        { { -0.5,  0.5/self.viewportSize.y*self.viewportSize.x, 0.0, 1.0 },  { 0.f, 0.f } },
+        
+        { {  0.5, -0.5/self.viewportSize.y*self.viewportSize.x, 0.0, 1.0 },  { 1.f, 1.f } },
+        { { -0.5,  0.5/self.viewportSize.y*self.viewportSize.x, 0.0, 1.0 },  { 0.f, 0.f } },
+        { {  0.5,  0.5/self.viewportSize.y*self.viewportSize.x, 0.0, 1.0 },  { 1.f, 0.f } },
+    };
+    self.vertices = [self.mtkView.device newBufferWithBytes:quadVertices
+                                                     length:sizeof(quadVertices)
+                                                    options:MTLResourceStorageModeShared]; // 创建顶点缓存
+    self.numVertices = sizeof(quadVertices) / sizeof(LYVertex); // 顶点个数
+}
+
+- (void)setupTexture {
+    UIImage *image = [UIImage imageNamed:@"abc"];
+    // 纹理描述符
+    MTLTextureDescriptor *textureDescriptor = [[MTLTextureDescriptor alloc] init];
+    textureDescriptor.pixelFormat = MTLPixelFormatRGBA8Unorm; // 图片的格式要和数据一致
+    textureDescriptor.width = image.size.width;
+    textureDescriptor.height = image.size.height;
+    textureDescriptor.usage = MTLTextureUsageShaderRead; // 原图片只需要读取
+    self.sourceTexture = [self.mtkView.device newTextureWithDescriptor:textureDescriptor]; // 创建纹理
+    
+    MTLRegion region = {{ 0, 0, 0 }, {image.size.width, image.size.height, 1}}; // 纹理上传的范围
+    Byte *imageBytes = [self loadImage:image];
+    if (imageBytes) { // UIImage的数据需要转成二进制才能上传，且不用jpg、png的NSData
+        [self.sourceTexture replaceRegion:region
+                        mipmapLevel:0
+                          withBytes:imageBytes
+                        bytesPerRow:4 * image.size.width];
+        free(imageBytes); // 需要释放资源
+        imageBytes = NULL;
+    }
+}
+
+- (void)setupThreadGroup {
+    self.groupSize = MTLSizeMake(16, 16, 1); // 太大某些GPU不支持，太小效率低；
+    
+    //保证每个像素都有处理到
+    _groupCount.width  = (self.sourceTexture.width  + self.groupSize.width -  1) / self.groupSize.width;
+    _groupCount.height = (self.sourceTexture.height + self.groupSize.height - 1) / self.groupSize.height;
+    _groupCount.depth = 1; // 我们是2D纹理，深度设为1
+}
+
+- (Byte *)loadImage:(UIImage *)image {
+    // 1获取图片的CGImageRef
+    CGImageRef spriteImage = image.CGImage;
+    
+    // 2 读取图片的大小
+    size_t width = CGImageGetWidth(spriteImage);
+    size_t height = CGImageGetHeight(spriteImage);
+    
+    Byte * spriteData = (Byte *) calloc(width * height * 4, sizeof(Byte)); //rgba共4个byte
+    
+    CGContextRef spriteContext = CGBitmapContextCreate(spriteData, width, height, 8, width * 4,
+                                                       CGImageGetColorSpace(spriteImage), kCGImageAlphaPremultipliedLast);
+    
+    // 3在CGContextRef上绘图
+    CGContextDrawImage(spriteContext, CGRectMake(0, 0, width, height), spriteImage);
+    
+    CGContextRelease(spriteContext);
+    
+    return spriteData;
+}
+
+
+- (void)setupRenderTarget {
+    CVMetalTextureCacheCreate(NULL, NULL, self.mtkView.device, NULL, &_textureCache); // 创建纹理缓存
+    
+    CFDictionaryRef empty; // empty value for attr value.
+    CFMutableDictionaryRef attrs;
+    empty = CFDictionaryCreate(kCFAllocatorDefault, // our empty IOSurface properties dictionary
+                               NULL,
+                               NULL,
+                               0,
+                               &kCFTypeDictionaryKeyCallBacks,
+                               &kCFTypeDictionaryValueCallBacks);
+    attrs = CFDictionaryCreateMutable(kCFAllocatorDefault,
+                                      1,
+                                      &kCFTypeDictionaryKeyCallBacks,
+                                      &kCFTypeDictionaryValueCallBacks);
+    
+    CFDictionarySetValue(attrs,
+                         kCVPixelBufferIOSurfacePropertiesKey,
+                         empty);
+    
+    CVPixelBufferRef renderTarget;
+    CVPixelBufferCreate(kCFAllocatorDefault, self.viewportSize.x, self.viewportSize.y,
+                        kCVPixelFormatType_32BGRA,
+                        attrs,
+                        &renderTarget);
+    // in real life check the error return value of course.
+    
+    
+    // rendertarget
+    {
+        size_t width = CVPixelBufferGetWidthOfPlane(renderTarget, 0);
+        size_t height = CVPixelBufferGetHeightOfPlane(renderTarget, 0);
+        MTLPixelFormat pixelFormat = MTLPixelFormatBGRA8Unorm;
+        
+        CVMetalTextureRef texture = NULL;
+        CVReturn status = CVMetalTextureCacheCreateTextureFromImage(NULL, self.textureCache, renderTarget, NULL, pixelFormat, width, height, 0, &texture);
+        if(status == kCVReturnSuccess)
+        {
+            self.destTexture = CVMetalTextureGetTexture(texture);
+            self.renderPixelBuffer = renderTarget;
+            CFRelease(texture);
+        }
+        else {
+            NSAssert(NO, @"CVMetalTextureCacheCreateTextureFromImage fail");
+        }
+    }
+}
+
+
+/**
+ *  根据CVPixelBufferRef返回图像
+ *
+ *  @param pixelBufferRef 像素缓存引用
+ *
+ *  @return UIImage对象
+ */
+- (UIImage *)lyGetImageFromPixelBuffer:(CVPixelBufferRef)pixelBufferRef {
+    CVImageBufferRef imageBuffer =  pixelBufferRef;
+    
+    CVPixelBufferLockBaseAddress(imageBuffer, 0);
+    void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
+    size_t width = CVPixelBufferGetWidth(imageBuffer);
+    size_t height = CVPixelBufferGetHeight(imageBuffer);
+    size_t bufferSize = CVPixelBufferGetDataSize(imageBuffer);
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(imageBuffer, 0); //
+    
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, baseAddress, bufferSize, NULL);
+    
+    // rgba的时候是kCGImageAlphaNoneSkipFirst | kCGBitmapByteOrderDefault，这样会导致出现蓝色的图片
+    CGImageRef cgImage = CGImageCreate(width, height, 8, 32, bytesPerRow, rgbColorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst, provider, NULL, true, kCGRenderingIntentDefault);
+    
+    UIImage *image = [UIImage imageWithCGImage:cgImage];
+    
+    CGImageRelease(cgImage);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(rgbColorSpace);
+    CVPixelBufferUnlockBaseAddress(imageBuffer, 0);
+    return image;
+}
+
+
+#pragma mark - delegate
+
+- (void)mtkView:(MTKView *)view drawableSizeWillChange:(CGSize)size {
+    self.viewportSize = (vector_uint2){size.width, size.height};
+}
+
+- (void)drawInMTKView:(MTKView *)view {    
+    // 每次渲染都要单独创建一个CommandBuffer
+    id<MTLCommandBuffer> commandBuffer = [self.commandQueue commandBuffer];
+    
+    // MTLRenderPassDescriptor描述一系列attachments的值，类似GL的FrameBuffer；同时也用来创建MTLRenderCommandEncoder
+    if(self.renderPassDescriptor)
+    {
+        self.renderPassDescriptor.colorAttachments[0].texture = self.mtkView.currentDrawable.texture;
+        self.renderPassDescriptor.colorAttachments[1].texture = self.destTexture;
+        
+        id<MTLRenderCommandEncoder> renderEncoder = [commandBuffer renderCommandEncoderWithDescriptor:self.renderPassDescriptor]; //编码绘制指令的Encoder
+        [renderEncoder setViewport:(MTLViewport){0.0, 0.0, self.viewportSize.x, self.viewportSize.y, -1.0, 1.0 }]; // 设置显示区域
+        [renderEncoder setRenderPipelineState:self.renderPipelineState]; // 设置渲染管道，以保证顶点和片元两个shader会被调用
+        
+        [renderEncoder setVertexBuffer:self.vertices
+                                offset:0
+                               atIndex:0]; // 设置顶点缓存
+        
+        [renderEncoder setFragmentTexture:self.sourceTexture
+                                  atIndex:0]; // 设置纹理
+        
+        [renderEncoder drawPrimitives:MTLPrimitiveTypeTriangle
+                          vertexStart:0
+                          vertexCount:self.numVertices]; // 绘制
+        
+        [renderEncoder endEncoding]; // 结束
+        
+        [commandBuffer presentDrawable:view.currentDrawable]; // 显示
+    }
+    
+    
+    [commandBuffer addCompletedHandler:^(id<MTLCommandBuffer> buffer) {
+        if (kCVReturnSuccess == CVPixelBufferLockBaseAddress(self.renderPixelBuffer,
+                                                             kCVPixelBufferLock_ReadOnly)) {
+            if (!self.imageView) {
+                dispatch_sync(dispatch_get_main_queue(), ^{
+                    UIImage *image = [self lyGetImageFromPixelBuffer:self.renderPixelBuffer];
+                    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds)/2, CGRectGetHeight(self.view.bounds)/2)];
+                    self.imageView.image = image;
+                    [self.view addSubview:self.imageView];
+                    [self.glView displayPixelBuffer:self.renderPixelBuffer];
+                });
+            }
+            CVPixelBufferUnlockBaseAddress(self.renderPixelBuffer, kCVPixelBufferLock_ReadOnly);
+        }
+    }];
+    
+    
+    [commandBuffer commit]; // 提交；
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
